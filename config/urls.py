11ee4f9f43a949722a755c6b3ab633d95035a6d2
Richie Mugambi <richiemugambi4@gmail.com>
2024-02-25 16:16:52 +0300

    feat: added auth route

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("drf_social_oauth2.urls", namespace="drf")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("notes.urls")),
