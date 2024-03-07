d4632d8ca0edf05017c4fc74ddd5c97d03d99b63
Richie Mugambi <richiemugambi4@gmail.com>
2024-03-07 17:03:37 +0300

    deps: added flake8 linter
    linted the whole project hence ensuting standards are met

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
-
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -165,5 +164,25 @@ SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {"fields": "id, name, email"}
 # TODO: IN PRODUCTION REMOVE AND USE ALLOWED ORIGINS
 CORS_ALLOW_ALL_ORIGINS: bool = True
 
+load_dotenv()
 
-from config.social_auth import *
+# Google configuration
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
+
+# Facebook configuration
+SOCIAL_AUTH_FACEBOOK_KEY = os.getenv("FACEBOOK_CLIENT_ID")
+SOCIAL_AUTH_FACEBOOK_SECRET = os.getenv("FACEBOOK_SECRET")
+
+# Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from Facebook.
+# Email is not sent by default, to get it, you must request the email permission.
+# SOCIAL_AUTH_FACEBOOK_SCOPE = ["email"]
+# SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {"fields": "id, name, email"}
+
+# Define SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE to get extra permissions from Google.
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+]
+
+SOCIAL_AUTH_RAISE_EXCEPTIONS = True
--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+# Create your models here
+
+
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=200, null=True, blank=True) # Slug for url
+    slug = models.SlugField(max_length=200, null=True, blank=True)   # Slug for url
 
     def __str__(self):
         return self.title
