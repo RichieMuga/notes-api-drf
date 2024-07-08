0cafb340f58ac99c293bfd4d2cefdf2c48d6c590
Richie Mugambi <richiemugambi4@gmail.com>
2024-07-09 00:06:32 +0300

    feat:(chore) add tests and made changes where necessary

--- notes/models.py
+++ notes/models.py
@@ -6,10 +6,13 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=200, null=True, blank=True)  # Slug for url
+    slug = models.SlugField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.NoteListCreate.as_view(), name='notes_list'),
+    path('v1/notes/', views.NoteListCreate.as_view(), name='notes_list'),
     path('notes/<int:pk>/', views.NoteRetriveUpdate.as_view(), name='notes_detail'),
     path('notes/<int:pk>/delete/', views.NoteRetrieveDestroy().as_view(), name='notes_delete'),
 ]
